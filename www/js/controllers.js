5378a1998cab696ac473c97351c73f323d92e336
Diego Carciente <diego.carciente@gmail.com>
2015-03-02 13:54:37 -0430

    Agregando uimagenes de planes Agregando descripcion por modal comentando la descripcion principal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,6 +4,25 @@ angular.module('starter.controllers', [])
     console.log('InitKey');
 })
 
-.controller('SectionsCtrl', function($scope, Sections) {
+.controller('SectionsCtrl', function($scope, Sections, $ionicModal, $window, $timeout) {
     $scope.sections = Sections.all();
+
+	$ionicModal.fromTemplateUrl('templates/modal-inicio.html', {
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal) {
+		$scope.modal = modal;
+		$scope.modalDragStart = { active: true, value: 0 }
+	})
+
+	$scope.openModal = function(modal) {
+		$scope.modal.show();
+	}
+
+	$scope.closeModal = function() {
+		return $scope.modal.hide();
+	};
+
 });
+
+
