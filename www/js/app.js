4303a9cbb40b258b5579e3894acc031d0a70b366
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-03-04 13:33:12 -0430

    cambio en url cambio en ruta init plugin config

--- www/js/app.js
+++ www/js/app.js
@@ -42,7 +42,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/keysystems');
+  $urlRouterProvider.otherwise('/section');
 
 })
 
